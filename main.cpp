7b03d39784734e9efc3f2d8a8a328077fd82a333
Izie Zahba <dushara.fahzad62@gmail.com>
2024-10-09 20:33:34 +0800

    Add main menu for navigation and readability.

--- main.cpp
+++ main.cpp
@@ -15,8 +15,44 @@ int main()
 	Words positiveWordsArray(positiveWordsTxt, SizeConstants::PositiveFileSize);
 	Words negativeWordsArray(negativeWordsTxt, SizeConstants::NegativeFileSize);
 
-    // reviewsCollection.calculateWordFrequencies(positiveWordsArray, negativeWordsArray);
-	reviewsCollection.analyzeSingleReview(positiveWordsArray, negativeWordsArray);
+	int choice = 0;
+
+	do
+	{
+
+		std::cout << "\nWhat do you want to do?\n";
+		std::cout << "1. Read Reviews with Sentiment Score\n";
+		std::cout << "2. Display Review Based on Total Count of Positive and Negative Words\n";
+		std::cout << "3. Exit\n";
+		std::cout << "Enter your choice: ";
+
+		if (!(std::cin >> choice) || choice < 1 || choice > 3)
+		{
+
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "Invalid choice! Choice should be between 1 to 3.\n";
+		}
+		else
+		{
+			switch (choice)
+			{
+			case 1:
+				reviewsCollection.analyzeSingleReview(positiveWordsArray, negativeWordsArray);
+				break;
+			case 2:
+				reviewsCollection.calculateWordFrequencies(positiveWordsArray, negativeWordsArray);
+				break;
+			case 3:
+				std::cout << "Exiting program. Hava a good day!\n";
+				break;
+			default:
+				std::cout << "Invalid choice. Please try again \n";
+				break;
+			}
+		}
+
+	} while (choice != 3);
 
 	return 0;
 }
